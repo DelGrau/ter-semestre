1c3e8f35fd2f6a4ae664a0d0370526a3cee93670
Alan Henrique Ribeiro <93848010+DelGrau@users.noreply.github.com>
2023-03-08 20:40:00 -0300

    Delete 1-Java directory

--- 1-Java/Java_intro-a-linguagem/Aula1.java
+++
@@ -1,9 +0,0 @@
-public class Aula1 {
-    public static void main(String[] args) {
-        if (10>20){
-            System.out.println("Ola, Mundo!");
-        } else {
-            System.out.println("Ola, Unipar!");
-        }
-    }
-}
